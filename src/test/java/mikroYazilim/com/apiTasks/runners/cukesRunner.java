f6482921e57abb4916079e6cb548d244ed93bce2
qaeulker <106735950+qaeulker@users.noreply.github.com>
2022-06-03 14:55:40 +0300

    Update cukesRunner.java

--- src/test/java/mikroYazilim/com/apiTasks/runners/cukesRunner.java
+++ src/test/java/mikroYazilim/com/apiTasks/runners/cukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "mikroYazilim/com/apiTasks/stepDefs",
         dryRun = false,
-        tags = "@nur"
+        tags = ""
 )
 
 
